67d2a22c1af5672d83963f3aa3a4ed2accce66c4
Emanuele Brotti <tipocippo@gmail.com>
2024-02-19 11:36:30 +0100

    scan for configs

--- walke-/JuliaModules/wlconfig.jl
+++ walke-/JuliaModules/wlconfig.jl
@@ -1,12 +1,33 @@
 module wlconfig
     export optimized, stop
-    optimized = false #if enabled, adds less entities (removes the outputs and inputs in each gate) but makes the code more difficult to read
-    stop = false #if enabled, adds a pause button to each gate. The button uses the #FFFFFF color
+    global optimized = false #if enabled, adds less entities (removes the outputs and inputs in each gate) but makes the code more difficult to read
+    global stop = false #if enabled, adds a pause button to each gate. The button uses the #FFFFFF color
+
+    ConfigArray = String[]
 
     function resetconfig()
-        optimized = false
-        stop = false
+        Main.optimized = false
+        Main.stop = false
     end
 
-    
+
+
+    function startconfig() 
+        ListOfFiles = readdir("ListOfConfigs", join=true)
+        for file in ListOfFiles
+            Lines = open(file) do f #read all lines
+                readlines(f)
+            end
+            for line in Lines
+                line = split(line, "-")[1] #remove comments
+                line = replace(line, " "=>"") #remove spaces
+                if (isempty(line)) #skips if empty
+                    continue
+                end
+                
+                push!(ConfigArray, uppercase(line)) #adds line to instructionarray
+            end
+        end
+    end
+    startconfig()
 end
---
+++ walke-/tempCodeRunnerFile.jl
@@ -0,0 +1,1 @@
+filter(contains(r".wlk-$"),
